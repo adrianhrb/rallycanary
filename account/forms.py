d91c7ed27a2c3861bc70de99d6c0539c99cdaa3b
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-02 20:56:58 +0000

    User's profile model created and registration from added

---
+++ account/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repite la contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
